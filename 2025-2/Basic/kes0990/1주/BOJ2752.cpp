f1c1849ec66ebc5e09d0acb3a0be09f27d62170b
kes0990 <kes0990@jbnu.ac.kr>
2025-09-19 23:06:16 +0900

    Create BOJ2752.cpp

---
+++ 2025-2/Basic/kes0990/1주/BOJ2752.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    int num[3];
+    for(int i=0; i<3; i++){
+        cin>>num[i];
+    }
+    for(int i=0; i<2; i++){
+        for(int j=i+1; j<3; j++){
+            if(num[i]>num[j]){
+                int tmp = num[i];
+                num[i]=num[j];
+                num[j]=tmp;
+            }
+        }
+    }
+    
+    for(int i=0; i<3; i++){
+        cout<<num[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
