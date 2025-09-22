a49ba7ff9f9effc07f1b473e8d821a0b8b0a6572
kes0990 <kes0990@jbnu.ac.kr>
2025-09-22 17:32:24 +0900

    Update BOJ2562.cpp

--- 2025-2/Basic/kes0990/1주/BOJ2562.cpp
+++ 2025-2/Basic/kes0990/1주/BOJ2562.cpp
@@ -7,7 +7,9 @@ int main(void){
     int arr[9];
     int max=0,x=0;
     for(int i=0; i<9; i++){
+        //수 배열에 저장
         cin>>arr[i];
+        //값 비교하며 최대값과 그 인덱스 저장
         if(arr[i]>max){max=arr[i]; x=i+1;}
     }
 
